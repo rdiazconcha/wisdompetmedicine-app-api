f64921e67ae0e0e126476d89a1f226e08b8c1bc4
Rodrigo Díaz Concha <rodrigo@rdiazconcha.com>
2022-09-30 16:51:10 +0200

    sw

--- actions/entrypoint.sh
+++ actions/entrypoint.sh
@@ -1,10 +1,8 @@
-#!/bin/bash
 set -e
 
-api_url="https://swapi.dev/api/people/${INPUT_POKEMON_ID}"
+api_url="https://swapi.dev/api/people/${INPUT_PERSON_ID}"
 echo $api_url
 
-pokemon_name=$(curl "${api_url}" | jq ".name")
-echo $pokemon_name
-
-echo "::set-output name=pokemon_name::$pokemon_name"
+person_name=$(curl "${api_url}" | jq ".name")
+echo $person_name
+echo "::set-output name=person_name::$person_name"
